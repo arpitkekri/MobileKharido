2a942c73703930c86038552d0a61327d6de486f7
arpitkekri <jainarpitkekri@gmail.com>
2021-08-04 12:44:10 +0530

    install fontawesome dependencies using yarn add --dev @fortawesome/fontawesome-free

--- src/App.js
+++ src/App.js
@@ -6,22 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="row">
+          
+        </div>
+      </div>
   );
 }
 
