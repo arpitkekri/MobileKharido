79fefab9ba7f5a4737ee525d152db12ca9a98100
arpitkekri <jainarpitkekri@gmail.com>
2021-08-04 12:25:04 +0530

    import bootstrap in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import package/distribution folder {dist} /file location/ filename
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
     <div className="App">
