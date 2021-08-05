58f76b537400ae0dda331bf1bff33d7b5a0b5055
arpitkekri <jainarpitkekri@gmail.com>
2021-08-05 21:29:05 +0530

    add serve using yarn add serve to serve build version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../logo.svg'
+import styled from 'styled-components'
 
 export default class Navbar extends Component {
     render() {
@@ -20,12 +21,32 @@ export default class Navbar extends Component {
                     </li>
                 </ul>
                 <Link to="/cart" className = "ml-auto"> 
-                    <button>
-                        <i className="fas fa-cart-plus" />
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus" />
+                        </span>
                         my cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
             </nav>
         )
     }
 }
+
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  outline-color: red;
+  cursor:pointer;
+  display: inline-block;
+  margin:0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover{
+    background:var(--lightBlue);
+    color:var(--mainBlue);
+  }
+`
