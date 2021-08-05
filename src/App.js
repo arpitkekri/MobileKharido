ec5a9906e9e8039988df5448b14e4a4dd2b30a63
arpitkekri <jainarpitkekri@gmail.com>
2021-08-05 11:51:57 +0530

    Install react router using yarn add react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import './App.css';
 
 // import package/distribution folder {dist} /file location/ filename
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
 
 class App extends Component {
   render() {
     return (
         <React.Fragment>
-          <h3>Hello from App</h3>
+          <Navbar></Navbar>
+          <ProductList></ProductList>
+          <Details></Details>
+          <Cart></Cart>
+          <Default></Default>
         </React.Fragment>
     );
   }
