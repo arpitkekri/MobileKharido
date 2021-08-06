3db67d0daf32e7fa36db65fbb5d37563e33ccb22
arpitkekri <jainarpitkekri@gmail.com>
2021-08-06 20:36:31 +0530

    use title component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
-import Product from "./Product"
+import React, { Component } from 'react';
+import Product from "./Product";
+import Title from './Title';
+import { storeProducts } from '../data.js';
 export default class ProductList extends Component {
+    // “state”, an object that determines how that component renders & behaves. 
+    // In other words, “state” is what allows you to create components that are dynamic and interactive.
+    state={
+        Products: storeProducts
+    }
     render() {
+        console.log(this.state.Products)
         return (
-            <div>
-                <h3>Hello from ProductList</h3>
-                <Product />
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                <div className="container">
+                    <Title name="our" title="products"  />
+                    {/* Product Row */}
+                    <div className="row" />
+                </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
