d22f731219ee9636b626ec626da4e1eccfc0f67a
arpitkekri <jainarpitkekri@gmail.com>
2021-08-07 01:44:16 +0530

    Seting up Product List using Context Api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import Product from "./Product";
 import Title from './Title';
-import { storeProducts } from '../data.js';
+// import { storeProducts } from '../data.js';
+import styled from 'styled-components';
+import { ProductConsumer } from '../context';
 export default class ProductList extends Component {
-    // “state”, an object that determines how that component renders & behaves. 
-    // In other words, “state” is what allows you to create components that are dynamic and interactive.
-    state={
-        Products: storeProducts
-    }
-    render() {
-        console.log(this.state.Products)
-        return (
-            <React.Fragment>
-                <div className="py-5">
-                <div className="container">
-                    <Title name="our" title="products"  />
-                    {/* Product Row */}
-                    <div className="row" />
-                </div>
-                </div>
-            </React.Fragment>
-        )
-    }
+  // “state”, an object that determines how that component renders & behaves. 
+  // In other words, “state” is what allows you to create components that are dynamic and interactive.
+//   state={
+//       Products: storeProducts
+//   };
+  render() {
+    return ( 
+      <React.Fragment>
+        <ProductWrapper className="py-5">
+        <div className="container">
+          <Title name="our" title="products"  />
+          {/* Product Row */}
+          <div className="row">
+          <ProductConsumer>
+            {value=>{
+                return value.products.map( product => {
+                    return <Product key={product.id} product={product} />;
+                });
+            }}
+          </ProductConsumer>
+          </div>
+        </div>
+        </ProductWrapper>
+      </React.Fragment>
+    )
+  }
 }
+const ProductWrapper = styled.section``;
--- src/context.js
+++ src/context.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
-
+import { storeProducts, detailProduct } from './data';
 // Create a context object -> it comes with 2 components 1) Provider 2) Consumer
 const ProductContext = React.createContext();
 
 
 class ProductProvider extends Component {
+    state={
+        products: storeProducts,
+        detailProduct: detailProduct
+    }
+    handleDetail = () => {
+        console.log("Hello from details");
+    };
+    addToCart = () => {
+        console.log("Hello from add to cart");
+    };
     render() {
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{
+                // destructuring array by spread operator ...
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
         )
