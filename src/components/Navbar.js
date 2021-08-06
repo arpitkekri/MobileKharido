dd9017cb0fcafa8becf94a94ccda29e7cf5a8d34
arpitkekri <jainarpitkekri@gmail.com>
2021-08-06 17:36:38 +0530

    Seting up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,21 @@ import { ButtonContainer } from './Button';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className = "navbar navbar-expand-sm bg-primary navbar-dark px-sm">
+            <NavWrapper className = "navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
                 https://www.iconfinder.com/Makoto_msk */}
                 <Link to = "/">
                     <img src = { logo } alt="Jain Mobile Point" className = "navbar-brand"/>
                 </Link>
-                <ul className="navbar-nav allign-items-center">
+                <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
                         <Link to = "/" className = "nav-link">
-                            Products
+                            products
                         </Link>
                     </li>
                 </ul>
-                <Link to="/cart" className = "ml-auto"> 
+                <Link to="/cart" className = "ml-auto">
                     <ButtonContainer>
                         <span className="mr-2">
                             <i className="fas fa-cart-plus" />
@@ -28,10 +28,20 @@ export default class Navbar extends Component {
                         my cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    // 1 rem = 16px
+    text-transform:capitalize;
+  }
+`;
 
 // const ButtonContainer = styled.button`
 //   text-transform: capitalize;
