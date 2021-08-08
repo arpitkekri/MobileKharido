3503e162ac3eacf94737aabd095634b90248288f
arpitkekri <jainarpitkekri@gmail.com>
2021-08-08 20:05:01 +0530

    setting detail of single product for testing

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,67 +1,70 @@
-import React, { Component } from 'react';
-import { ProductConsumer } from '../context';
-import { Link } from 'react-router-dom';
-import { ButtonContainer } from './Button.js';
+import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button.js";
 export default class Details extends Component {
-    render() {
-        return (
-            <ProductConsumer>
-                { value => {
-                    // destructuring ...
-                    const {id, company, img, info, price, title, inCart } = value.detailProduct;
-                    return (
-                        <div className="container py-5">
-                            {/* title */}
-                            <div className="row">
-                                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
-                                    <h1> { title } </h1>
+  render() {
+    return (
+      <ProductConsumer>
+        {(value) => {
+          // destructuring ...
+          const { id, company, img, info, price, title, inCart } =
+            value.detailProduct;
+          return (
+            <div className="container py-5">
+              {/* title */}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1> {title} </h1>
+                </div>
+              </div>
+              {/* end title */}
 
-                                </div>
-                            </div>
-                            {/* end title */}
+              {/* product info */}
+              <div className="row">
+                {/* product image */}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <img src={img} alt="product image" className="img-fluid" />
+                </div>
 
-                            {/* product info */}
-                            <div className="row">
-                                {/* product image */}
-                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                                    <img src = { img } alt = "product image" className = "img-fluid" />
-                                </div>
+                {/* product text */}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2> model : {title} </h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    made by :{" "}
+                    <span className="text-uppercase"> {company} </span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      price : <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    some info about the product:
+                  </p>
+                  <p className="text-muted lead"> {info} </p>
 
-                                {/* product text */}
-                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                                    <h2> model : { title } </h2>
-                                    <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                                        made by : <span className="text-uppercase"> { company } </span>
-                                    </h4>
-                                    <h4 className="text-blue">
-                                        <strong> 
-                                            price : <span>$</span> 
-                                            { price }
-                                        </strong>
-                                    </h4>
-                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                                        some info about the product:
-                                    </p>
-                                    <p className="text-muted lead"> { info } </p>
-                                    
-                                    {/* buttons */}
-                                    <div>
-                                        <Link to = '/'><ButtonContainer>back to product</ButtonContainer></Link>
-                                        <ButtonContainer 
-                                        disabled = { inCart ? true:false }
-                                        onClick = {() => {
-                                            value.addToCart();
-                                        }}
-                                        >
-                                            {inCart ? "inCart": "add to cart" }
-                                        </ButtonContainer>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }}
-            </ProductConsumer>
-        )
-    }
+                  {/* buttons */}
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer>back to product</ButtonContainer>
+                    </Link>
+                    <ButtonContainer
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addToCart();
+                      }}
+                    >
+                      {inCart ? "inCart" : "add to cart"}
+                    </ButtonContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
+  }
 }
