54c784317336ce6d6ca9a45cff4ed131bc568612
arpitkekri <jainarpitkekri@gmail.com>
2021-08-08 20:12:03 +0530

    setting detail of single product for testing

--- src/components/Details.js
+++ src/components/Details.js
@@ -31,7 +31,7 @@ export default class Details extends Component {
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
                   <h2> model : {title} </h2>
                   <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                    made by :{" "}
+                    made by :
                     <span className="text-uppercase"> {company} </span>
                   </h4>
                   <h4 className="text-blue">
