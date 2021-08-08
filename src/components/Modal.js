f898ab80b9b46b811b3b9f8770fe4cb4940eb057
arpitkekri <jainarpitkekri@gmail.com>
2021-08-08 23:59:43 +0530

    modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <div>
+               <h1>Hello from modal</h1>
+            </div>
+        )
+    }
+}
