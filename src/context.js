acf464e4dfda6e467614b022cf705c1c963c332b
arpitkekri <jainarpitkekri@gmail.com>
2021-08-10 01:23:23 +0530

    cart is complete with decrement method setup

--- src/context.js
+++ src/context.js
@@ -68,10 +68,32 @@ class ProductProvider extends Component {
       });
   };
   increment = (id) => {
-      console.log("This is increment method");
+      let tempCart = [...this.state.cart];
+      const selectedProduct = tempCart.find(item => item.id === id);
+      const index = tempCart.indexOf(selectedProduct);
+      const product = tempCart[index];
+      product.count = product.count + 1;
+      product.total = product.count * product.price;
+      this.setState(()=>{return{cart: [...tempCart]}}, () => {
+        this.addTotals();
+      });
   }
   decrement = (id) => {
-    console.log("This is decrement method");
+      let tempCart = [...this.state.cart];
+      const selectedProduct = tempCart.find(item => item.id === id);
+      const index = tempCart.indexOf(selectedProduct);
+      const product = tempCart[index];
+      product.count = product.count - 1;
+      
+      if(product.count === 0) {
+        this.removeItem(id);
+      }
+      else {
+        product.total = product.count * product.price;
+        this.setState(()=>{return{cart: [...tempCart] }}, () => {
+          this.addTotals();
+        });
+      }
   }
   removeItem = (id) => {
       let tempProduct = [...this.state.products];
