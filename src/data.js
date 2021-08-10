375b9a9d1bb3fee5a228c31a75814fb3bb239705
arpitkekri <jainarpitkekri@gmail.com>
2021-08-11 03:18:04 +0530

    changes data

--- src/data.js
+++ src/data.js
@@ -3,10 +3,10 @@ export const storeProducts = [
     id: 1,
     title: "Google Pixel - Black",
     img: "img/product-1.png",
-    price: 10,
+    price: 56999,
     company: "GOOGLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "4 GB RAM | 32 GB ROM | 12.7 cm (5 inch) Full HD Display | 12.3MP Rear Camera | 8MP Front Camera | 2770 mAh Battery | Qualcomm Snapdragon 821 64-bit Quad Core 2.15GHz Processor | Single Sim | Bottom Firing Speaker, 3 Microphones (2 Front, 1 Rear), Adaptive Audio Amplifier | 1920 x 1080 Full HD | 16:9 Aspect Ratio, Material: Anodised Aluminium (Corning Gorilla Glass 4), Oleophobic Coating, RGB LED Notification Light ",
     inCart: false,
     count: 0,
     total: 0
@@ -15,10 +15,10 @@ export const storeProducts = [
     id: 2,
     title: "Samsung S7",
     img: "img/product-2.png",
-    price: 16,
+    price: 30499,
     company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "4 GB RAM | 32 GB ROM | Expandable Upto 200 GB | 13.97 cm (5.5 inch) Quad HD Display | 12MP Rear Camera | 5MP Front Camera | 3600 mAh Li-Ion Battery | Exynos 8890 Processor | Dual Sim | 2560 X 1440 Pixels Quad HD Display | Super AMOLED Screen | Android Marshmallow 6 | Octa Core | clock speed 2.3 GHz | Dual Pixel, Auto Focus, Motion Mode, Panorama Mode, f/1.7, Hyperlapse, Slow Motion, Pro-mode, Live Broadcast, Video Collage, Virtual Shot, Food Mode",
     inCart: false,
     count: 0,
     total: 0
@@ -27,10 +27,10 @@ export const storeProducts = [
     id: 3,
     title: "HTC 10 - Black",
     img: "img/product-3.png",
-    price: 8,
+    price: 48999,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "4 GB RAM | 32 GB ROM | The mobile measures 145.9 mm x 71.9 mm x 9.0 mm; and weighs around 161 grams. The mobile from HTC features 5.2 inches (13.21 cm) display that has a resolution of 1440 x 2560 pixels | It is equipped with Octa core (2.2 GHz, Quad core, Kryo + 1.5 GHz, Quad core, Kryo) Qualcomm Snapdragon 820 MSM8996 | HTC 10 runs Android v6.0 (Marshmallow) operating system and is packed with 3000 mAh battery | 12 MP camera | 5 MP front camera | Light sensor, Proximity sensor, Accelerometer, Compass, Gyroscope",
     inCart: false,
     count: 0,
     total: 0
@@ -39,10 +39,10 @@ export const storeProducts = [
     id: 4,
     title: "HTC 10 - White",
     img: "img/product-4.png",
-    price: 18,
+    price: 50425,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "4 GB RAM | 32 GB ROM | Color: favorite white | The mobile measures 145.9 mm x 71.9 mm x 9.0 mm; and weighs around 161 grams. The mobile from HTC features 5.2 inches (13.21 cm) display that has a resolution of 1440 x 2560 pixels | It is equipped with Octa core (2.2 GHz, Quad core, Kryo + 1.5 GHz, Quad core, Kryo) Qualcomm Snapdragon 820 MSM8996 | HTC 10 runs Android v6.0 (Marshmallow) operating system | 3000 mAh battery | 12 MP camera | 5 MP front camera | Light sensor, Proximity sensor, Accelerometer, Compass, Gyroscope",
     inCart: false,
     count: 0,
     total: 0
@@ -51,10 +51,10 @@ export const storeProducts = [
     id: 5,
     title: "HTC Desire 626s",
     img: "img/product-5.png",
-    price: 24,
+    price: 21625,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "",
     inCart: false,
     count: 0,
     total: 0
@@ -63,7 +63,7 @@ export const storeProducts = [
     id: 6,
     title: "Vintage Iphone",
     img: "img/product-6.png",
-    price: 17,
+    price: 45000,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
@@ -75,7 +75,7 @@ export const storeProducts = [
     id: 7,
     title: "Iphone 7",
     img: "img/product-7.png",
-    price: 30,
+    price: 20499,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
@@ -87,7 +87,7 @@ export const storeProducts = [
     id: 8,
     title: "Smashed Iphone",
     img: "img/product-8.png",
-    price: 2,
+    price: 6459,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
@@ -101,10 +101,10 @@ export const detailProduct = {
   id: 1,
   title: "Google Pixel - Black",
   img: "img/product-1.png",
-  price: 10,
-  company: "google",
+  price: 56999,
+  company: "GOOGLE",
   info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    "4 GB RAM | 32 GB ROM | 12.7 cm (5 inch) Full HD Display | 12.3MP Rear Camera | 8MP Front Camera | 2770 mAh Battery | Qualcomm Snapdragon 821 64-bit Quad Core 2.15GHz Processor | Single Sim | Bottom Firing Speaker, 3 Microphones (2 Front, 1 Rear), Adaptive Audio Amplifier | 1920 x 1080 Full HD | 16:9 Aspect Ratio, Material: Anodised Aluminium (Corning Gorilla Glass 4), Oleophobic Coating, RGB LED Notification Light ",
   inCart: false,
   count: 0,
   total: 0
