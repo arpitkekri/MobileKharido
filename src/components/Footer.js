26ece8802efa774aa65be34b675dbf29f6da6aaa
arpitkekri <jainarpitkekri@gmail.com>
2021-09-08 15:40:39 +0530

    github icon link update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer className="page-footer font-small mt-auto footer">
-                <a href="https://github.com/arpitkekri/Mobile-Point" target="_blank" rel="noreferrer" className="float-btn" data-toggle="tooltip" title="Github repo">
+                <a href="https://github.com/arpitkekri/MobileKharido" target="_blank" rel="noreferrer" className="float-btn" data-toggle="tooltip" title="Github repo">
                 <i className="fab fa-github my-float-btn" />
                 </a>
             </footer>
